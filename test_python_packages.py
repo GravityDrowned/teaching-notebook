8195160a4415bef09c3ce85c5f02cf37cdd8ef63
Nicolas M. Thiéry <nthiery@users.sf.net>
2021-02-03 23:18:35 +0100

    Check nbgrader version + PEP 8 + mypy config

--- test_python_packages.py
+++ test_python_packages.py
@@ -1,7 +1,14 @@
-def test_python_version():
+def test_python_version() -> None:
     import sys
     assert sys.version_info.major == 3
     assert sys.version_info.minor == 8
 
-def test_tensorflow():
-    import tensorflow
+
+def test_tensorflow() -> None:
+    import tensorflow  # type: ignore
+    tensorflow
+
+
+def test_nbgrader() -> None:
+    import nbgrader    # type: ignore
+    assert nbgrader.version_info == (0, 7, 0, 'dev')
