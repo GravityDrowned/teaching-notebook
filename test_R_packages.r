dee0330cb9839e8b3311bbf4384135dd1b5a8fb4
Nicolas M. Thiéry <nthiery@users.sf.net>
2021-02-04 09:25:02 +0100

    Specify which mirror to use (the federated one)
    https://stackoverflow.com/questions/11488174/how-to-select-a-cran-mirror-in-r

--- test_R_packages.r
+++ test_R_packages.r
@@ -10,6 +10,12 @@ library("psych")
 # These packages are not available through conda; check that they can
 # be installed by the user:
 
+## Default repo
+local({r <- getOption("repos")
+       r["CRAN"] <- "https://cloud.r-project.org" 
+       options(repos=r)
+})
+
 install.packages("metRology", keep_outputs=TRUE, verbose=TRUE)
 install.packages("psycho",    keep_outputs=TRUE, verbose=TRUE)
 install.packages("rAverage",  keep_outputs=TRUE, verbose=TRUE)
