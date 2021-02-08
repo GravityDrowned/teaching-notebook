9d04e4a3875ca9e92845c530de863f4d10b6c7c6
Nicolas M. Thiéry <nthiery@users.sf.net>
2021-02-08 19:27:25 +0100

    metrology not installed after all

--- test_R_packages.r
+++ test_R_packages.r
@@ -6,7 +6,6 @@ library("ggdist")
 library("FactoMineR")
 library("psych")
 #library("magick")
-library("metRology")
 
 # These packages are not available through conda; check that they can
 # be installed by the user:
@@ -20,9 +19,11 @@ local({r <- getOption("repos")
 install.packages("psycho",    keep_outputs=TRUE, verbose=TRUE)
 install.packages("rAverage",  keep_outputs=TRUE, verbose=TRUE)
 install.packages("RWiener",   keep_outputs=TRUE, verbose=TRUE)
+install.packages("metRology", keep_outputs=TRUE, verbose=TRUE)
 
 # Test that these manually installed R packages are available and functional
 
 library("psycho")
 library("rAverage")
 library("RWiener")
+library("metRology")
