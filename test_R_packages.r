53b6e7433fbe5200e2d892eadc5b10e567ec4ac4
Nicolas M. Thiéry <nthiery@users.sf.net>
2021-02-08 10:29:26 +0100

    Check that metRology is actually preinstalled

--- test_R_packages.r
+++ test_R_packages.r
@@ -6,6 +6,7 @@ library("ggdist")
 library("FactoMineR")
 library("psych")
 #library("magick")
+library("metRology")
 
 # These packages are not available through conda; check that they can
 # be installed by the user:
@@ -16,14 +17,12 @@ local({r <- getOption("repos")
        options(repos=r)
 })
 
-install.packages("metRology", keep_outputs=TRUE, verbose=TRUE)
 install.packages("psycho",    keep_outputs=TRUE, verbose=TRUE)
 install.packages("rAverage",  keep_outputs=TRUE, verbose=TRUE)
 install.packages("RWiener",   keep_outputs=TRUE, verbose=TRUE)
 
-# Test that these manually R packages are available and functional
+# Test that these manually installed R packages are available and functional
 
-library("metRology")
 library("psycho")
 library("rAverage")
 library("RWiener")
