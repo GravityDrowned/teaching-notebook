23aff83ccd09147104c6b235b74dc3ebb1e515da
Nicolas M. Thiéry <nthiery@users.sf.net>
2021-09-19 21:14:37 +0200

    Added basic tests for C++/cling

---
+++ test_cpp_packages.cpp
@@ -0,0 +1,10 @@
+#include <xeus/xeus.hpp>
+#include <xeus-cling/xeus_cling_config.hpp>
+#include <xwidgets/xwidget.hpp>
+#include <info-111.hpp>
+#include <laby/laby.hpp>
+#include <laby/global_fr.hpp>
+
+int main() {
+  return 1;
+}
