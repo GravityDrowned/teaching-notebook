cacef72f74917d77dc9df4120db853e07ab8e346
Nicolas M. Thiéry <nthiery@users.sf.net>
2021-09-19 23:36:54 +0200

    Check availability of xcpp/xmime.hpp

--- test_cpp_packages.cpp
+++ test_cpp_packages.cpp
@@ -1,6 +1,7 @@
 #include <xeus/xeus.hpp>
 #include <xeus-cling/xeus_cling_config.hpp>
 #include <xwidgets/xwidget.hpp>
+#include <xcpp/xmime.hpp>
 #include <info-111.hpp>
 #include <laby/laby.hpp>
 #include <laby/global_fr.hpp>
